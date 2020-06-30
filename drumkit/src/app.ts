75b3e24bbaa019ed73357ce19a9e8445511c0823
jacekpodsiadlo <jp2252000@gmail.com>
2020-07-01 00:21:34 +0200

    drumkit uploaded

--- drumkit/src/app.ts
+++ drumkit/src/app.ts
@@ -10,9 +10,69 @@ export class App {
         this.start();
     }
     start() {
-        document.addEventListener('keydown', (e) => { this.onKeyDown(e) } )
+        document.addEventListener('keydown', (e) => { this.onKeyDown(e) } );
         document.querySelector('#playBtn').addEventListener('click', () => { this.playChannel1() } );
         document.querySelector('#recBtn').addEventListener('click', (e: MouseEvent) => { this.recChannel1(e) } );
+        document.addEventListener('keyup', (e) => { this.onKeyUp(e) } );
+       }
+    onKeyUp(e: KeyboardEvent){
+        const key = e.key;
+        for(let i = 0; i<key.length; i++){
+            let first = document.getElementById('one');
+            let second = document.getElementById('two');
+            let third = document.getElementById('three');
+            let fourth = document.getElementById('four');
+            let fifth = document.getElementById('five');
+            let sixth = document.getElementById('six');
+            let seventh = document.getElementById('seven');
+            let eighth = document.getElementById('eight');
+            let ninth = document.getElementById('nine');
+            if(key==='q'){
+                first.style.backgroundColor = 'black';
+                first.style.height = '50px';
+                first.style.width = '100px';
+            }
+            if(key==='w'){
+                second.style.backgroundColor = 'black'
+                second.style.height = '50px';
+                second.style.width = '100px';
+            }
+            if(key==='e'){
+                third.style.backgroundColor = 'black';
+                third.style.height = '50px';
+                third.style.width = '100px';
+            }
+            if(key==='r'){
+                fourth.style.backgroundColor = 'black';
+                fourth.style.height = '50px';
+                fourth.style.width = '100px';
+            }
+            if(key==='t'){
+                fifth.style.backgroundColor = 'black';
+                fifth.style.height = '50px';
+                fifth.style.width = '100px';
+            }
+            if(key==='y'){
+                sixth.style.backgroundColor = 'black';
+                sixth.style.height = '50px';
+                sixth.style.width = '100px';
+            }
+            if(key==='u'){
+                seventh.style.backgroundColor = 'black';
+                seventh.style.height = '50px';
+                seventh.style.width = '100px';
+            }
+            if(key==='i'){
+                eighth.style.backgroundColor = 'black';
+                eighth.style.height = '50px';
+                eighth.style.width = '100px';
+            }
+            if(key==='o'){
+                ninth.style.backgroundColor = 'black';
+                ninth.style.height = '50px';
+                ninth.style.width = '100px';
+            }
+        }
     }
     recChannel1(e: MouseEvent): void {
         this.recStartTime = e.timeStamp;
@@ -33,6 +93,62 @@ export class App {
         const key = e.key;
         const time = e.timeStamp - this.recStartTime;
         let audioId: string;
+        for(let i = 0; i<key.length; i++){
+            let first = document.getElementById('one');
+            let second = document.getElementById('two');
+            let third = document.getElementById('three');
+            let fourth = document.getElementById('four');
+            let fifth = document.getElementById('five');
+            let sixth = document.getElementById('six');
+            let seventh = document.getElementById('seven');
+            let eighth = document.getElementById('eight');
+            let ninth = document.getElementById('nine');
+            if(key==='q'){
+                first.style.backgroundColor = 'pink';
+                first.style.height = '60px';
+                first.style.width = '120px';
+            }
+            if(key==='w'){
+                second.style.backgroundColor = 'pink'
+                second.style.height = '60px';
+                second.style.width = '120px';
+            }
+            if(key==='e'){
+                third.style.backgroundColor = 'pink';
+                third.style.height = '60px';
+                third.style.width = '120px';
+            }
+            if(key==='r'){
+                fourth.style.backgroundColor = 'pink';
+                fourth.style.height = '60px';
+                fourth.style.width = '120px';
+            }
+            if(key==='t'){
+                fifth.style.backgroundColor = 'pink';
+                fifth.style.height = '60px';
+                fifth.style.width = '120px';
+            }
+            if(key==='y'){
+                sixth.style.backgroundColor = 'pink';
+                sixth.style.height = '60px';
+                sixth.style.width = '120px';
+            }
+            if(key==='u'){
+                seventh.style.backgroundColor = 'pink';
+                seventh.style.height = '60px';
+                seventh.style.width = '120px';
+            }
+            if(key==='i'){
+                eighth.style.backgroundColor = 'pink';
+                eighth.style.height = '60px';
+                eighth.style.width = '120px';
+            }
+            if(key==='o'){
+                ninth.style.backgroundColor = 'pink';
+                ninth.style.height = '60px';
+                ninth.style.width = '120px';
+            }
+        }
         switch(key) {
             case 'q':
                 audioId = 'boomAudio';
@@ -78,5 +194,6 @@ export class App {
         }
         this.channel1.push(sound);
         }
+      
     }
 }
